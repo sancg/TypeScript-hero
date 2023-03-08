0cc3563c131e52eccdc6b04d989cd8540ac95158
sancg <sanblackx1@gmail.com>
2023-03-08 14:50:01 -0500

    📝 Added: Objects as funct parameters

--- src/03_functions.ts
+++ src/03_functions.ts
@@ -12,4 +12,16 @@
 
   const product = createProductToJSON('P1', new Date());
   console.log(product);
+
+  // Objects as parameters
+  type LoginType = {
+    email: string;
+    password: string;
+  };
+  const login = (data: LoginType) => {
+    const { email, password } = data;
+    console.log(email);
+  };
+
+  login({ email: 'pepa@platzi.com', password: 'pedro123' });
 })();
