5c464d49f5b563689801352809f3510133452dec
sancg <sanblackx1@gmail.com>
2023-03-08 15:56:28 -0500

    ✨ Enhancement: Refactoring Export methods

--- src/products/product.model.ts
+++ src/products/product.model.ts
@@ -1,8 +1,10 @@
 // All related to type definitions
-export type Size = 'S' | 'M' | 'L' | 'XL';
-export type Product = {
+type Size = 'S' | 'M' | 'L' | 'XL';
+type Product = {
   name: string;
   createdAt: Date;
   stock: number;
   size?: Size;
 };
+
+export { Size, Product };
--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -1,12 +1,13 @@
 import { Product } from './product.model';
 
 // .service <name_convention> relates to the Methods or treatment of Data
-export const products: Product[] = [];
-export const addProduct = (data: Product) => {
+const products: Product[] = [];
+const addProduct = (data: Product) => {
   products.push(data);
 };
 
-export const inStock = (): number => {
-  const total = products.reduce((prev, current) => prev + current.stock, 0);
-  return total;
+const inStock = (): number => {
+  return products.reduce((prev, current) => prev + current.stock, 0);
 };
+
+export { products, addProduct, inStock };
