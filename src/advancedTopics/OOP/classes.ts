e8be161c21e8c4e532912359a8998be32ab331b2
Santiago Cano <sanblackx1@gmail.com>
2023-03-30 05:03:33 -0400

    🍻 PowerUp: Variables scope w/ public, private

--- src/advancedTopics/OOP/classes.ts
+++ src/advancedTopics/OOP/classes.ts
@@ -4,11 +4,15 @@ type CustomDate = {
   day: number;
 };
 
-class myDate {
+export class myDate {
   date: CustomDate;
+  /* Access to variables */
+  private year: number;
+
   /* Constructor method as indicates, is where all variables can be initialized */
   constructor(date: CustomDate) {
     this.date = date;
+    this.year = date.year;
   }
 
   /* Creating Methods */
@@ -21,3 +25,16 @@ class myDate {
 const customDate = new myDate({ year: 2002, month: 12, day: 1 });
 console.log(customDate.date);
 customDate.printFormat();
+
+/* Viewing accessibility errors when declaring private properties */
+class myClass {
+  private year: number;
+
+  constructor(year: number) {
+    this.year = year;
+  }
+}
+
+const useClass = new myClass(2023);
+// ❌ This would cause and accessible variable error
+//console.log(useClass.year);
